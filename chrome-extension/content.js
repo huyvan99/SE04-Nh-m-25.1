e430ae17c0f36cb8e02e88c1a4a7a4b499ba031c
loongchu <49190441+loongchu@users.noreply.github.com>
2021-01-01 21:46:07 +0700

    Update content.js
    Fix lỗi lấy nhầm comment

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,13 +1,16 @@
 console.log("Content is running");
+var posts = [];
+var comments = [];
+var users = [];
 function scrollDown() {
     window.scrollBy(0, 10000);
 }
 function getPost() {
     console.log("getPost running")
-    postContent = document.getElementsByClassName("kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql ii04i59q");
-    console.log(postContent.length, " bài post lấy được");
+    posts = document.getElementsByClassName("kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql ii04i59q");
+    console.log(posts.length, " bài post lấy được");
     for (let post of postContent) {
-        console.log(post.textContent);
+        console.log(posts.textContent);
     }
 }
 function getComment() {
@@ -15,10 +18,8 @@ function getComment() {
     comments = document.getElementsByClassName("kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql");
     console.log(comments.length, " số comment");
     for (var i = 0; i < comments.length; i++) {
-        // Only if there is only single class
         if (comments[i].className == 'kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql') {
-            // Do something with the element e[i]
-            console.log(comments[i].textContent)
+            console.log(comments[i].textContent);
         }
     }
 }
@@ -27,10 +28,8 @@ function getUser() {
     users = document.getElementsByClassName("pq6dq46d");
     console.log(users.length, " người comment số bài viết của bạn");
     for (var i = 0; i < users.length; i++) {
-        // Only if there is only single class
         if (users[i].className == 'pq6dq46d') {
-            // Do something with the element e[i]
-            console.log(users[i].textContent)
+            console.log(users[i].textContent);
         }
     }
 }
@@ -38,20 +37,29 @@ function getUserAndComment() {
     console.log("getUserandComment running")
     users = document.getElementsByClassName("pq6dq46d");
     comments = document.getElementsByClassName("kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql");
-    console.log(users.length, comments.length)
-    console.log(postContent.length);
+    console.log(users.length, comments.length);
+    // console.log(posts.length);
+    let cmt = [];
+    let usert = [];
     for (var i = 0; i < users.length; i++) {
-        // Only if there is only single class
-        if (users[i].className == 'pq6dq46d' && comments[i].className == 'kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql') {
-            // Do something with the element e[i]
-            console.log(users[i].textContent, ": ", commentContent[i].textContent)
+        if (users[i].className == 'pq6dq46d') {
+            usert.push(users[i]);
         }
+    }
+    for (var i = 0; i < comments.length; i++) {
+        if (comments[i].className == 'kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql') {
+            cmt.push(comments[i]);
+        }
+    }
+    console.log(usert.length, cmt.length);
+    for (var i = 0; i < usert.length; i++) {
+        console.log(usert[i].textContent,": ",cmt[i].textContent);  
     }
 }
 window.setTimeout(getPost, 10000);
 window.setTimeout(getComment, 15000);
 window.setTimeout(getUser, 15000);
-window.setTimeout(getUserAndComment,15000);
+window.setTimeout(getUserAndComment, 15000);
 console.log("kiin")
 // Bắt comment
 
