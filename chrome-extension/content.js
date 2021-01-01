921321148ee44e00aafb93a56645d18e79a59043
=kienvu <=vutrungkien_t62@hus.edu.vn>
2021-01-01 21:36:40 +0700

    Lấy thông tin của từng comment theo bài post

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -21,12 +21,37 @@ function getComment() {
             console.log(comments[i].textContent)
         }
     }
-    // for (let comment of commentContent) {
-    //     console.log(comment.textContent);
-    // }
+}
+function getUser() {
+    console.log("getUser running")
+    users = document.getElementsByClassName("pq6dq46d");
+    console.log(users.length, " người comment số bài viết của bạn");
+    for (var i = 0; i < users.length; i++) {
+        // Only if there is only single class
+        if (users[i].className == 'pq6dq46d') {
+            // Do something with the element e[i]
+            console.log(users[i].textContent)
+        }
+    }
+}
+function getUserAndComment() {
+    console.log("getUserandComment running")
+    users = document.getElementsByClassName("pq6dq46d");
+    comments = document.getElementsByClassName("kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql");
+    console.log(users.length, comments.length)
+    console.log(postContent.length);
+    for (var i = 0; i < users.length; i++) {
+        // Only if there is only single class
+        if (users[i].className == 'pq6dq46d' && comments[i].className == 'kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql') {
+            // Do something with the element e[i]
+            console.log(users[i].textContent, ": ", commentContent[i].textContent)
+        }
+    }
 }
 window.setTimeout(getPost, 10000);
 window.setTimeout(getComment, 15000);
+window.setTimeout(getUser, 15000);
+window.setTimeout(getUserAndComment,15000);
 console.log("kiin")
 // Bắt comment
 
